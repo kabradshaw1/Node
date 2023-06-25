5790ee4743d1ccb4d08cd703245943ef0a320f70
kabradshaw1 <itllbok82@gmail.com>
2023-06-24 21:25:55 -0400

    working on getting npx graphql-codegen to work, probably my authentication that is stopping it

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,4 +1,7 @@
-import typeDefs from './typeDefs';
+import path from 'path';
+import fs from 'fs';
 import resolvers from './resolvers';
+
+const typeDefs = fs.readFileSync(path.join(__dirname, './typeDefs.graphql'), 'utf8');
 
 export { typeDefs, resolvers };
