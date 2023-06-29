e6a83f8c47039414b283d4873586fe76030ce626
kabradshaw1 <itllbok82@gmail.com>
2023-06-29 13:50:52 -0400

    .graphql files are fixed.  graphql-codegen now running in terminal

--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -23,10 +23,31 @@ export type Auth = {
   user?: Maybe<User>;
 };
 
+export type Comment = {
+  __typename?: 'Comment';
+  _id?: Maybe<Scalars['ID']['output']>;
+  commentBody?: Maybe<Scalars['String']['output']>;
+  createdAt?: Maybe<Scalars['String']['output']>;
+  username?: Maybe<Scalars['String']['output']>;
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
+  addComment?: Maybe<Post>;
+  addPost?: Maybe<Post>;
   addUser?: Maybe<Auth>;
   login?: Maybe<Auth>;
+};
+
+
+export type MutationAddCommentArgs = {
+  PostId: Scalars['ID']['input'];
+  commentBody: Scalars['String']['input'];
+};
+
+
+export type MutationAddPostArgs = {
+  postText: Scalars['String']['input'];
 };
 
 
@@ -42,15 +63,45 @@ export type MutationLoginArgs = {
   password: Scalars['String']['input'];
 };
 
+export type Post = {
+  __typename?: 'Post';
+  _id?: Maybe<Scalars['ID']['output']>;
+  commentCount?: Maybe<Scalars['Int']['output']>;
+  comments?: Maybe<Array<Maybe<Comment>>>;
+  createdAt?: Maybe<Scalars['String']['output']>;
+  postText?: Maybe<Scalars['String']['output']>;
+  username?: Maybe<Scalars['String']['output']>;
+};
+
 export type Query = {
   __typename?: 'Query';
+  me?: Maybe<User>;
+  post?: Maybe<Post>;
+  posts?: Maybe<Array<Maybe<Post>>>;
   user?: Maybe<User>;
+  users?: Maybe<Array<Maybe<User>>>;
+};
+
+
+export type QueryPostArgs = {
+  _id: Scalars['ID']['input'];
+};
+
+
+export type QueryPostsArgs = {
+  username?: InputMaybe<Scalars['String']['input']>;
+};
+
+
+export type QueryUserArgs = {
+  username: Scalars['String']['input'];
 };
 
 export type User = {
   __typename?: 'User';
   _id?: Maybe<Scalars['ID']['output']>;
   email?: Maybe<Scalars['String']['output']>;
+  posts?: Maybe<Array<Maybe<Post>>>;
   username?: Maybe<Scalars['String']['output']>;
 };
 
@@ -69,7 +120,43 @@ export type AddUserMutationVariables = Exact<{
 }>;
 
 
-export type AddUserMutation = { __typename?: 'Mutation', addUser?: { __typename?: 'Auth', token?: string | null, user?: { __typename?: 'User', _id?: string | null, username?: string | null } | null } | null };
+export type AddUserMutation = { __typename?: 'Mutation', addUser?: { __typename?: 'Auth', token?: string | null } | null };
+
+export type AddPostMutationVariables = Exact<{
+  poestText: Scalars['String']['input'];
+}>;
+
+
+export type AddPostMutation = { __typename?: 'Mutation', addPost?: { __typename?: 'Post', _id?: string | null, postText?: string | null, createdAt?: string | null, username?: string | null, commentCount?: number | null, comments?: Array<{ __typename?: 'Comment', _id?: string | null } | null> | null } | null };
+
+export type AddCommentMutationVariables = Exact<{
+  PostId: Scalars['ID']['input'];
+  commentBody: Scalars['String']['input'];
+}>;
+
+
+export type AddCommentMutation = { __typename?: 'Mutation', addComment?: { __typename?: 'Post', _id?: string | null, commentCount?: number | null, comments?: Array<{ __typename?: 'Comment', _id?: string | null, commentBody?: string | null, createdAt?: string | null, username?: string | null } | null> | null } | null };
+
+export type PostQueryVariables = Exact<{
+  id: Scalars['ID']['input'];
+}>;
+
+
+export type PostQuery = { __typename?: 'Query', post?: { __typename?: 'Post', _id?: string | null, postText?: string | null, createdAt?: string | null, username?: string | null, commentCount?: number | null, comments?: Array<{ __typename?: 'Comment', _id?: string | null, createdAt?: string | null, username?: string | null, commentBody?: string | null } | null> | null } | null };
+
+export type PostsQueryVariables = Exact<{
+  username?: InputMaybe<Scalars['String']['input']>;
+}>;
+
+
+export type PostsQuery = { __typename?: 'Query', posts?: Array<{ __typename?: 'Post', _id?: string | null, postText?: string | null, createdAt?: string | null, username?: string | null, commentCount?: number | null, comments?: Array<{ __typename?: 'Comment', _id?: string | null, username?: string | null, createdAt?: string | null, commentBody?: string | null } | null> | null } | null> | null };
+
+export type UserQueryVariables = Exact<{
+  username: Scalars['String']['input'];
+}>;
+
+
+export type UserQuery = { __typename?: 'Query', user?: { __typename?: 'User', _id?: string | null, username?: string | null, email?: string | null, posts?: Array<{ __typename?: 'Post', _id?: string | null, postText?: string | null, createdAt?: string | null, commentCount?: number | null } | null> | null } | null };
 
 
 export const LoginDocument = gql`
@@ -114,10 +201,6 @@ export const AddUserDocument = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
   addUser(username: $username, email: $email, password: $password) {
     token
-    user {
-      _id
-      username
-    }
   }
 }
     `;
@@ -149,3 +232,217 @@ export function useAddUserMutation(baseOptions?: Apollo.MutationHookOptions<AddU
 export type AddUserMutationHookResult = ReturnType<typeof useAddUserMutation>;
 export type AddUserMutationResult = Apollo.MutationResult<AddUserMutation>;
 export type AddUserMutationOptions = Apollo.BaseMutationOptions<AddUserMutation, AddUserMutationVariables>;
+export const AddPostDocument = gql`
+    mutation addPost($poestText: String!) {
+  addPost(postText: $poestText) {
+    _id
+    postText
+    createdAt
+    username
+    commentCount
+    comments {
+      _id
+    }
+  }
+}
+    `;
+export type AddPostMutationFn = Apollo.MutationFunction<AddPostMutation, AddPostMutationVariables>;
+
+/**
+ * __useAddPostMutation__
+ *
+ * To run a mutation, you first call `useAddPostMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useAddPostMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [addPostMutation, { data, loading, error }] = useAddPostMutation({
+ *   variables: {
+ *      poestText: // value for 'poestText'
+ *   },
+ * });
+ */
+export function useAddPostMutation(baseOptions?: Apollo.MutationHookOptions<AddPostMutation, AddPostMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<AddPostMutation, AddPostMutationVariables>(AddPostDocument, options);
+      }
+export type AddPostMutationHookResult = ReturnType<typeof useAddPostMutation>;
+export type AddPostMutationResult = Apollo.MutationResult<AddPostMutation>;
+export type AddPostMutationOptions = Apollo.BaseMutationOptions<AddPostMutation, AddPostMutationVariables>;
+export const AddCommentDocument = gql`
+    mutation addComment($PostId: ID!, $commentBody: String!) {
+  addComment(PostId: $PostId, commentBody: $commentBody) {
+    _id
+    commentCount
+    comments {
+      _id
+      commentBody
+      createdAt
+      username
+    }
+  }
+}
+    `;
+export type AddCommentMutationFn = Apollo.MutationFunction<AddCommentMutation, AddCommentMutationVariables>;
+
+/**
+ * __useAddCommentMutation__
+ *
+ * To run a mutation, you first call `useAddCommentMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useAddCommentMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [addCommentMutation, { data, loading, error }] = useAddCommentMutation({
+ *   variables: {
+ *      PostId: // value for 'PostId'
+ *      commentBody: // value for 'commentBody'
+ *   },
+ * });
+ */
+export function useAddCommentMutation(baseOptions?: Apollo.MutationHookOptions<AddCommentMutation, AddCommentMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<AddCommentMutation, AddCommentMutationVariables>(AddCommentDocument, options);
+      }
+export type AddCommentMutationHookResult = ReturnType<typeof useAddCommentMutation>;
+export type AddCommentMutationResult = Apollo.MutationResult<AddCommentMutation>;
+export type AddCommentMutationOptions = Apollo.BaseMutationOptions<AddCommentMutation, AddCommentMutationVariables>;
+export const PostDocument = gql`
+    query post($id: ID!) {
+  post(_id: $id) {
+    _id
+    postText
+    createdAt
+    username
+    commentCount
+    comments {
+      _id
+      createdAt
+      username
+      commentBody
+    }
+  }
+}
+    `;
+
+/**
+ * __usePostQuery__
+ *
+ * To run a query within a React component, call `usePostQuery` and pass it any options that fit your needs.
+ * When your component renders, `usePostQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = usePostQuery({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function usePostQuery(baseOptions: Apollo.QueryHookOptions<PostQuery, PostQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<PostQuery, PostQueryVariables>(PostDocument, options);
+      }
+export function usePostLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<PostQuery, PostQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<PostQuery, PostQueryVariables>(PostDocument, options);
+        }
+export type PostQueryHookResult = ReturnType<typeof usePostQuery>;
+export type PostLazyQueryHookResult = ReturnType<typeof usePostLazyQuery>;
+export type PostQueryResult = Apollo.QueryResult<PostQuery, PostQueryVariables>;
+export const PostsDocument = gql`
+    query posts($username: String) {
+  posts(username: $username) {
+    _id
+    postText
+    createdAt
+    username
+    commentCount
+    comments {
+      _id
+      username
+      createdAt
+      commentBody
+    }
+  }
+}
+    `;
+
+/**
+ * __usePostsQuery__
+ *
+ * To run a query within a React component, call `usePostsQuery` and pass it any options that fit your needs.
+ * When your component renders, `usePostsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = usePostsQuery({
+ *   variables: {
+ *      username: // value for 'username'
+ *   },
+ * });
+ */
+export function usePostsQuery(baseOptions?: Apollo.QueryHookOptions<PostsQuery, PostsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<PostsQuery, PostsQueryVariables>(PostsDocument, options);
+      }
+export function usePostsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<PostsQuery, PostsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<PostsQuery, PostsQueryVariables>(PostsDocument, options);
+        }
+export type PostsQueryHookResult = ReturnType<typeof usePostsQuery>;
+export type PostsLazyQueryHookResult = ReturnType<typeof usePostsLazyQuery>;
+export type PostsQueryResult = Apollo.QueryResult<PostsQuery, PostsQueryVariables>;
+export const UserDocument = gql`
+    query user($username: String!) {
+  user(username: $username) {
+    _id
+    username
+    email
+    posts {
+      _id
+      postText
+      createdAt
+      commentCount
+    }
+  }
+}
+    `;
+
+/**
+ * __useUserQuery__
+ *
+ * To run a query within a React component, call `useUserQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUserQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUserQuery({
+ *   variables: {
+ *      username: // value for 'username'
+ *   },
+ * });
+ */
+export function useUserQuery(baseOptions: Apollo.QueryHookOptions<UserQuery, UserQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<UserQuery, UserQueryVariables>(UserDocument, options);
+      }
+export function useUserLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<UserQuery, UserQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<UserQuery, UserQueryVariables>(UserDocument, options);
+        }
+export type UserQueryHookResult = ReturnType<typeof useUserQuery>;
+export type UserLazyQueryHookResult = ReturnType<typeof useUserLazyQuery>;
+export type UserQueryResult = Apollo.QueryResult<UserQuery, UserQueryVariables>;
