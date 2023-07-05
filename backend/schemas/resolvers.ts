2ff4fae05245daa1dc273b6d30d6389773a3fe89
kabradshaw1 <itllbok82@gmail.com>
2023-07-05 18:37:56 -0400

    type errors in server are gone

--- backend/schemas/resolvers.ts
+++ backend/schemas/resolvers.ts
@@ -1,7 +1,6 @@
 import { AuthenticationError } from "apollo-server-express";
 import { UserModel, User } from '../models/User';
 import { PostModel } from '../models/Post';
-import { CommentModel } from "../models/Comment";
 import { signToken } from "../utils/auth";
 import {
   MutationAddUserArgs,
@@ -13,33 +12,21 @@ import {
   QueryPostArgs,
   QueryPostsArgs,
 
-  Resolvers,
   ResolversParentTypes,
   ResolversTypes,
-  Maybe
+  Maybe,
 } from '../generated/graphql';
 
-
-const transformDoc = (doc: any) => {
-  return {
-    _id: doc._id.toString(),
-    username: doc.username,
-    email: doc.email,
-    postText: doc.postText,
-    createdAt: doc.createdAt,
-    commentBody: doc.commentBody
-    // posts: doc.posts.map((post: any) => transformDoc(post))
-  };
-};
 
 interface Context {
   user?: Maybe<User>;
 }
 
 
-const resolvers: Resolvers = {
+
+const resolvers = {
   Query: {
-    me: async (parent: ResolversParentTypes['Query'], context: Context): Promise<ResolversTypes['User']> => {
+    me: async (parent: ResolversParentTypes['Query'], context: Context) => {
       if (context.user) {
         const userData = await UserModel.findOne({ _id: context.user._id })
           .select('-__v -password')
@@ -48,20 +35,19 @@ const resolvers: Resolvers = {
         if (!userData) {
           throw new Error('User not found');
         }
-
-        return transformDoc(userData);
+        return userData;
       }
 
       throw new AuthenticationError('You need to be logged in!');
     },
-    user: async (parent: ResolversParentTypes['Query'], args: QueryUserArgs): Promise<ResolversTypes['User']> => {
+    user: async (parent: ResolversParentTypes['Query'], args: QueryUserArgs) => {
       const user = await UserModel.findOne({ username: args.username }).select('-__v -password').populate('posts');
 
       if (!user) {
         throw new Error('User not found');
       }
 
-      return transformDoc(user);
+      return user;
     },
     users: async (parent: ResolversParentTypes['Query']): Promise<ResolversTypes['User'][]> => {
       return UserModel.find()
@@ -71,24 +57,23 @@ const resolvers: Resolvers = {
     post: async (parent: ResolversParentTypes['Query'], args: QueryPostArgs): Promise<ResolversTypes['Post'] | null> => {
       return PostModel.findOne({_id: args._id})
     },
-    posts: async (parent: ResolversParentTypes['Query'], args: QueryPostsArgs): Promise<ResolversTypes['Post'][]> => {
+    posts: async (parent: ResolversParentTypes['Query'], args: QueryPostsArgs) => {
       const params = args.username ? { username: args.username } : {};
       const posts = await PostModel.find(params).sort({ createdAt: -1 });
 
-      // Transform each post document to match the expected shape
-      return posts.map(post => transformDoc(post));
+      return posts;
     }
 
 
   },
   Mutation: {
-    addUser: async (parent: ResolversParentTypes['Mutation'], args: MutationAddUserArgs): Promise<ResolversTypes['Auth'] > => {
+    addUser: async (parent: ResolversParentTypes['Mutation'], args: MutationAddUserArgs) => {
       const user = await UserModel.create(args);
       const token = signToken(user);
 
-      return { token, user: transformDoc(user) };
+      return { token, user };
     },
-    login: async (parent: ResolversParentTypes['Mutation'], { email, password }: MutationLoginArgs): Promise<ResolversTypes['Auth']> => {
+    login: async (parent: ResolversParentTypes['Mutation'], { email, password }: MutationLoginArgs) => {
       const user = await UserModel.findOne({ email });
 
       if (!user) {
@@ -103,9 +88,9 @@ const resolvers: Resolvers = {
 
       const token = signToken(user);
 
-      return { token, user: transformDoc(user) };
+      return { token, user };
     },
-    addPost: async (parent: ResolversParentTypes['Mutation'], args: MutationAddPostArgs, context: Context): Promise<ResolversTypes['Post']> => {
+    addPost: async (parent: ResolversParentTypes['Mutation'], args: MutationAddPostArgs, context: Context) => {
       if(context.user) {
         const post = await PostModel.create({ ...args, username: context.user.username });
 
@@ -115,21 +100,16 @@ const resolvers: Resolvers = {
           { new: true }
         );
 
-        return transformDoc(post);
+        return post;
       }
 
       throw new AuthenticationError('You need to be logged in!');
     },
-    addComment: async (parent: ResolversParentTypes['Mutation'], args: MutationAddCommentArgs, context: Context): Promise<ResolversTypes['Post']> => {
+    addComment: async (parent: ResolversParentTypes['Mutation'], args: MutationAddCommentArgs, context: Context) => {
       if(context.user) {
-        const comment = new CommentModel({
-          commentBody: args.commentBody,
-          username: context.user.username
-        });
-
         const updatedPost = await PostModel.findOneAndUpdate(
           {_id: args.PostId },
-          { $push: { comments: comment } },
+          { $push: { comments: { body: args.commentBody, username: context.user.username } } },
           { new: true, runValidators: true }
         );
 
@@ -137,7 +117,7 @@ const resolvers: Resolvers = {
           throw new Error('Post not found');
         }
 
-        return transformDoc(updatedPost);
+        return updatedPost;
       }
 
       throw new AuthenticationError('You need to be logged in!');
