d27d32fad339dd78bc11cf78a0b867257700680f
kabradshaw1 <itllbok82@gmail.com>
2023-07-11 17:02:06 -0400

    trying to add route to EventForm

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -11,8 +11,8 @@ import authSlice from '../../store/slices/authSlice';
 
 export default function Header() {
 
-  const isLoggedIn = useSelector((state: RootState) => state.auth.user?.username);
-
+  const isLoggedIn = useSelector((state: RootState) => state.auth.user);
+  console.log(isLoggedIn?.isAdmin)
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -37,7 +37,7 @@ export default function Header() {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <NavDropdown title={isLoggedIn ? `${isLoggedIn}`: 'Welcome'} id="basic-nav-dropdown">
+                  <NavDropdown title={isLoggedIn ? `${isLoggedIn.username}`: 'Welcome'} id="basic-nav-dropdown">
                     {isLoggedIn
                       ? <LinkContainer to='/'>
                           <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
@@ -54,14 +54,16 @@ export default function Header() {
                     <LinkContainer to='/profile'>
                       <NavDropdown.Item>Profile</NavDropdown.Item>
                     </LinkContainer>
-
-                    <NavDropdown.Item href="#action/3.2">
-                      Free Space
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">
                       Fill Me!
                     </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    {isLoggedIn?.isAdmin
+                      ? <LinkContainer to='/event_form'>
+                          <NavDropdown.Item>Event Form</NavDropdown.Item>
+                        </LinkContainer>
+                      : null
+                    }
                   </NavDropdown>
                 </Nav>
               </Navbar.Collapse>
---
+++ frontend/src/page/EventForm.tsx
@@ -0,0 +1,7 @@
+const EventForm: React.FC = () => {
+  return (
+    <h1>Event form placeholder</h1>
+  )
+};
+
+export default EventForm
