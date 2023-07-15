4a8accc8fec3ab002f53fafdd2ea65df20e3f6dd
kabradshaw1 <itllbok82@gmail.com>
2023-07-14 22:47:13 -0400

    working on mirating over to aws-sdk 3

--- backend/utils/bucket.ts
+++ backend/utils/bucket.ts
@@ -1,12 +1,17 @@
-import AWS from 'aws-sdk';
+import { S3Client } from "@aws-sdk/client-s3";
 
-export const bucket = 'dev-gql-s3-bucket';
-export const bucketEndpoint = 'http://localhost:9000';
+const isProduction = process.env.PRODUCTION === 'true';
 
-export const s3 = new AWS.S3({
-  endpoint: bucketEndpoint,
-  accessKeyId: 'ly1y6iMtYf',
-  secretAccessKey: 'VNcmMuDARGGstqzkXF1Van1Mlki5HGU9',
-  sslEnabled: false,
-  s3ForcePathStyle: true,
-});
+export const bucket = isProduction ? process.env.BUCKET_NAME : 'dev-gql-s3-bucket';
+export const bucketEndpoint = 'http://localhost:9000',
+const s3Config = isProduction
+  ? { region: process.env.REGION }
+  : {
+      endpoint: bucketEndpoint,
+      credentials: {
+        accessKeyId: 'ly1y6iMtYf',
+        secretAccessKey: 'VNcmMuDARGGstqzkXF1Van1Mlki5HGU9',
+      },
+    };
+
+export const s3 = new S3Client(s3Config);
--- backend/utils/signedURL.ts
+++ backend/utils/signedURL.ts
@@ -1,40 +1,30 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { bucket, s3 } from './bucket';
 
-const generateUploadURL = async (prop: string, fileName: string): Promise<string> => {
-  const params = {
+const generateUploadURL = async (prop: string, fileName: string, fileType: string): Promise<string> => {
+  const command = new PutObjectCommand({
     Bucket: prop,
     Key: fileName,
-    Expires: 60,
-  };
-  try {
-    const url = await s3.getSignedUrlPromise('putObject', params);
-    console.log('The URL is', url);
-    return url;
-  } catch(err) {
-    console.log('error getting signed url', err);
-    throw err;
-  }
-};
-
-export const generateProdUploadURL = (fileName: string) => {
-  if (process.env.BUCKET_NAME)
-  return generateUploadURL(process.env.BUCKET_NAME, fileName);
-};
-
-export const generateDevUploadURL = async (fileName: string, fileType: string): Promise<string>  => {
-  const params = {
-    Bucket: bucket,
-    Key: fileName,
-    Expires: 60,
     ContentType: fileType
-  };
+  });
+
   try {
-    const url = await s3.getSignedUrlPromise('putObject', params);
-    console.log('The URL is', url);
-    return url;
+    const signedUrl = await getSignedUrl(s3, command, { expiresIn: 3600 });
+    console.log('The URL is', signedUrl);
+    return signedUrl;
   } catch(err) {
     console.log('error getting signed url', err);
     throw err;
   }
 };
 
+export const generateProdUploadURL = (fileName: string, fileType: string) => {
+  if (bucket)
+    return generateUploadURL(bucket, fileName, fileType);
+};
+
+export const generateDevUploadURL = async (fileName: string, fileType: string)  => {
+  if (bucket)
+    return generateUploadURL(bucket, fileName, fileType)
+};
