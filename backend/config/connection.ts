891960d22a9e5777ad0ce3a4da5a08cea3524f18
kabradshaw1 <itllbok82@gmail.com>
2023-07-22 23:05:51 -0400

    going my database over to atlas

--- backend/config/connection.ts
+++ backend/config/connection.ts
@@ -1,23 +1,8 @@
 import mongoose from 'mongoose';
 
-const mongoUsernameAndPassword = process.env.MONGO_USERNAME && process.env.MONGO_PASSWORD
-    ? `${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@`
-    : '';
+if (process.env.MONGO)
 
-let connectionString = `mongodb://${mongoUsernameAndPassword}${process.env.MONGO_HOST}:27017/${process.env.MONGO_DB_NAME}`;
-
-if(mongoUsernameAndPassword){
-    connectionString += '?authSource=admin';
-}
-
-// const connectionOptions = process.env.USE_SSL === 'true' ? {
-//   tls: true,
-//   tlsCAFile: '/etc/certs/chain.pem', // Path to the CA certificate
-//   tlsCertificateKeyFile: '/etc/certs/mongodb.pem' // Path to the MongoDB server/client certificate
-// } : {};
-
-
-mongoose.connect(connectionString, /*connectionOptions*/);
+mongoose.connect(process.env.MONGO);
 
 const db = mongoose.connection;
 
