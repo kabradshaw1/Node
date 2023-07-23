c9eae108801d54a45099a236a720db4a14c3f18a
kabradshaw1 <itllbok82@gmail.com>
2023-07-22 20:37:09 -0400

    address address checker to event form and address with link to google maps to event list

--- frontend/src/components/EventList/EventCard/index.tsx
+++ frontend/src/components/EventList/EventCard/index.tsx
@@ -4,7 +4,10 @@ import Card from 'react-bootstrap/Card';
 interface SingleEventProp {
   data: Event | null
 }
+
 const EventCard: React.FC<SingleEventProp> = ({ data: event }) => {
+
+  const addressLink = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(event?.address || '')}`;
 
   return (
     <Card>
@@ -12,7 +15,7 @@ const EventCard: React.FC<SingleEventProp> = ({ data: event }) => {
       <Card.Body>
         {event?.signedURL ? <Card.Img src={event.signedURL}/> : null}
         {event?.description ? <Card.Text>{event.description}</Card.Text> : null}
-        <Card.Text>{event?.date}</Card.Text>
+        {event?.address ? <Card.Text>This event will be held at <a href={addressLink} target="_blank" rel="noopener noreferrer">{event.address}</a> on {event?.date}</Card.Text> : null}
       </Card.Body>
     </Card>
   )
